fae8385da17a74e4b5bb2a15bac6891f90e4707a
Belarmino <belarminom890@gmail.com>
2023-07-05 09:16:22 -0600

    CALCULADOR EXAMPLE

---
+++ Operaciones.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Operaciones
+    {
+        public double Sumar(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Restar(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Multiplicar(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Dividir(double num1, double num2)
+        {
+            if (num2 != 0)
+            {
+                return num1 / num2;
+            }
+            else
+            {
+                Console.WriteLine("Error: No se puede dividir por cero.");
+                return num1; // Retornar el mismo número para mantener el resultado anterior
+            }
+        }
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Calculadora;
+using System;
+
+namespace CalculadoraConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear una instancia de la calculadora
+            Operaciones calculadora = new Operaciones();
+
+            double resultado = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Resultado: " + resultado);
+                Console.WriteLine("Elija una opción:");
+                Console.WriteLine("1. Suma");
+                Console.WriteLine("2. Resta");
+                Console.WriteLine("3. Multiplicación");
+                Console.WriteLine("4. División");
+                Console.WriteLine("5. Salir");
+
+
+                //Console.Write("Ingrese el número: ");
+                //double numero = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Elije la Opción: ");
+                int opcion = Convert.ToInt32(Console.ReadLine());
+               
+                if (opcion == 5)
+                {
+                    break;
+                }
+
+                Console.Write("Ingrese el número para la operacion: ");
+                double numero = Convert.ToDouble(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        resultado = calculadora.Sumar(resultado, numero);
+                        break;
+                    case 2:
+                        resultado = calculadora.Restar(resultado, numero);
+                        break;
+                    case 3:
+                        resultado = calculadora.Multiplicar(resultado, numero);
+                        break;
+                    case 4:
+                        resultado = calculadora.Dividir(resultado, numero);
+                        break;
+                    default:
+                        Console.WriteLine("Ingresa el numero");
+                        break;
+                }
+
+                Console.WriteLine("Resultado despues de la operacion es : " + resultado);
+                Console.WriteLine();
+            }
+
+        }
+    }
+    }
